66f4407f783166f4621a0f84245f0c11a8dac1ae
Mikael Koskinen <mijapkos@gmail.com>
2015-11-12 14:14:11 +0200

    Added portable class library.

--- src/NSQLFormatter.Portable/Properties/AssemblyInfo.cs
+++ src/NSQLFormatter.Portable/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -13,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Mikael Koskinen 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6caf8033-88e1-4850-bf18-acd712ddb5b9")]
 
 // Version information for an assembly consists of the following four values:
 //
